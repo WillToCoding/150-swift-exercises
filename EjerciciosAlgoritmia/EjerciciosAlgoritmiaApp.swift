90145c4b746ddb09f72bb3a0d85d1f0b42fa89ea
jcmoranmartin <jmm0109@alu.medac.es>
2025-12-05 23:51:53 +0100

    Initial Commit

---
+++ EjerciciosAlgoritmia/EjerciciosAlgoritmiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjerciciosAlgoritmiaApp.swift
+//  EjerciciosAlgoritmia
+//
+//  Created by Juan Carlos on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EjerciciosAlgoritmiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
